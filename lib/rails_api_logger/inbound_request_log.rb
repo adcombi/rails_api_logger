36dfe1ae8321059bae53cfbf113fea5ad4e78ebe
Diosa Taylor <null>
2022-07-15 16:38:40 +0200

    safe indifferent access with password hash

--- lib/rails_api_logger/inbound_request_log.rb
+++ lib/rails_api_logger/inbound_request_log.rb
@@ -3,7 +3,7 @@ class InboundRequestLog < RequestLog
 
   def request_body=(val)
     if val.with_indifferent_access.key? :password
-      val[:password] = BCrypt::Password.create(val[:password])
+      val[:password] = BCrypt::Password.create(val.with_indifferent_access[:password])
     end
     self[:request_body] = val
   end
