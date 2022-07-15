53d261dc98149cad42362f84d2e6cd4977f4e5dd
Diosa Taylor <null>
2022-07-15 16:26:08 +0200

    encrypt password in request body

--- lib/rails_api_logger/inbound_request_log.rb
+++ lib/rails_api_logger/inbound_request_log.rb
@@ -1,2 +1,9 @@
 class InboundRequestLog < RequestLog
+
+  def request_body=(val)
+    if val.with_indifferent_access.key? :password
+      val[:password] = BCrypt::Password.create(val[:password])
+      self[:request_body] = val
+    end
+  end
 end
