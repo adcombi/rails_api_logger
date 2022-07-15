06d77217fa47de6b713115ac49c686d36102bd2e
Diosa Taylor <null>
2022-07-15 16:33:42 +0200

    set request body as val if no password

--- lib/rails_api_logger/inbound_request_log.rb
+++ lib/rails_api_logger/inbound_request_log.rb
@@ -4,7 +4,7 @@ class InboundRequestLog < RequestLog
   def request_body=(val)
     if val.with_indifferent_access.key? :password
       val[:password] = BCrypt::Password.create(val[:password])
-      self[:request_body] = val
     end
+    self[:request_body] = val
   end
 end
