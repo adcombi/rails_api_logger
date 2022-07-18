29f8360d90844f3590752b4b210a24be94b9ca68
Diosa Taylor <null>
2022-07-18 10:51:53 +0200

    check request body is hash

--- lib/rails_api_logger/inbound_request_log.rb
+++ lib/rails_api_logger/inbound_request_log.rb
@@ -2,7 +2,7 @@ class InboundRequestLog < RequestLog
   require 'bcrypt'
 
   def request_body=(val)
-    if val.with_indifferent_access.key? :password
+    if val.is_a?(Hash) && (val.with_indifferent_access.key? :password)
       val[:password] = BCrypt::Password.create(val.with_indifferent_access[:password])
     end
     self[:request_body] = val
